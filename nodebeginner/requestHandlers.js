bfc2dc1b1b4610a4956150d8ea1b20e9f3c6930b
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-03-27 09:59:45 +0100

    Node Beginner

--- nodebeginner/requestHandlers.js
+++ nodebeginner/requestHandlers.js
@@ -1,4 +1,6 @@
-var exec = require( "child_process" ).exec;
+var querystring = require( "querystring" ),
+			 fs = require( "fs" ),
+	 formidable = require( "formidable" );
 
 function start( response ) {
 	console.log( "Request handler 'start' was called" );
@@ -8,9 +10,9 @@ function start( response ) {
 				'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
 				'</head>' +
 				'<body>' +
-				'<form action="/upload" method="post">' +
-				'<textarea name="text" rows="20" cols="60"></textarea>' +
-				'<input type="submit" value="Submit Text" />' +
+				'<form action="/upload" enctype="multipart/form-data" method="post">' +
+				'<input type="file" name="upload">' +
+				'<input type="submit" value="Upload File" />' +
 				'</form>' +
 				'</body>' +
 				'</html>';				
@@ -21,12 +23,43 @@ function start( response ) {
 
 }
 
-function upload( response, postData ) {
+function upload( response, request ) {
 	console.log( "Request handler 'uplaod' was called" );
-	response.writeHead( 200, { "Content-Type" : "text/plain" } );
-	response.write( "You've sent: " + postData );
-	response.end();
+
+	var form = new formidable.IncomingForm();
+	console.log( "About to parse" );
+	form.parse( request, function( error, fields, files ) {
+		console.log( "parsing done" );
+		fs.rename( files.upload.path, "/tmp/test.png", function( err ) {
+			if ( err ) {
+				fs.unlink( "/tmp/test.png" );
+				fs.rename( files.upload.path, "/tmp/test.png" );
+			}
+		});
+		response.writeHead( 200, { "Content-Type" : "text/html" } );
+		response.write( "Received image:<br />" );
+		response.write( "<img src='/show' />" );
+		response.end();
+	});
+
+}
+
+function show( response, request ) {
+	console.log( "Request handler 'show' was called." );
+	fs.readFile( "/tmp/test.png", "binary", function( error, file) {
+		if ( error ) {
+			response.writeHead( 500, { "Content-Type" : "text/plain" } );
+			response.write( error + "\n" );
+			response.end();
+		} else {
+			response.writeHead( 200, { "Content-Type" : "text/plain" } );
+			response.write( file, "binary" );
+			response.end();
+		}
+	});
+	
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
